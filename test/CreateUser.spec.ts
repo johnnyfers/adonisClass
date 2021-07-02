5ce852059c4b4e88962ed5b81a8a5f35a2e9caa0
joao <joaovlima07@hotmail.com>
2021-07-02 20:23:47 -0300

    wrinting my first tests

---
+++ test/CreateUser.spec.ts
@@ -0,0 +1,41 @@
+import test from 'japa'
+
+import User from '../app/Models/User'
+import Database from '@ioc:Adonis/Lucid/Database'
+
+
+test.group('Welcome', (group) => {
+
+    group.beforeEach(async () => {
+        await Database.beginGlobalTransaction()
+    })
+
+    group.afterEach(async () => {
+        await Database.rollbackGlobalTransaction()
+    })
+
+    test('create an user', async (assert) => {
+        const user = new User()
+
+        user.username = 'adonis.com'
+        user.email = 'virk@nisjss.com'
+        user.password = 'secret'
+
+        const saved = await user.save()
+
+        assert.equal(user.username, saved.username)
+    })
+
+    test('ensure user password gets hashed during save', async (assert) => {
+        const user = new User()
+
+        user.username = 'adonis.com'
+        user.email = 'virk@adonisjss.com'
+        user.password = 'secret'
+
+        await user.save()
+
+        assert.notEqual(user.password, 'secret')
+    })
+
+})
