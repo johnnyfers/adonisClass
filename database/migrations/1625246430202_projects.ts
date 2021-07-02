8601171d60405360af351c2eed829d1e3e72fc9a
joao <joaovlima07@hotmail.com>
2021-07-02 14:31:18 -0300

    feate: projects and tasks migrations

---
+++ database/migrations/1625246430202_projects.ts
@@ -0,0 +1,27 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Projects extends BaseSchema {
+  protected tableName = 'projects'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table
+        .integer('user_id')
+        .unsigned()
+        .references('id').inTable('users')
+        .onUpdate('CASCADE')
+        .onDelete('SET NULL')
+
+      table.string('title').notNullable()
+      table.string('description').notNullable()
+
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
---
+++ database/migrations/1625246439557_tasks.ts
@@ -0,0 +1,41 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Tasks extends BaseSchema {
+  protected tableName = 'tasks'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table
+        .integer('project_id')
+        .unsigned()
+        .references('id').inTable('users')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+        .notNullable()
+      table
+        .integer('user_id')
+        .unsigned()
+        .references('id').inTable('users')
+        .onUpdate('CASCADE')
+        .onDelete('SET NULL')
+      table
+        .integer('file_id')
+        .unsigned()
+        .references('id').inTable('users')
+        .onUpdate('CASCADE')
+        .onDelete('SET NULL')
+
+      table.string('title').notNullable()
+      table.string('description').notNullable()
+      table.timestamp('due_date')
+
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
