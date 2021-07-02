eb68cd87227a13b277e3c796e6f2cc3b373648dc
joao <joaovlima07@hotmail.com>
2021-07-02 10:34:50 -0300

    feat: reset password, auth compleated

--- app/Controllers/Http/ForgotPasswordsController.ts
+++ app/Controllers/Http/ForgotPasswordsController.ts
@@ -2,6 +2,8 @@ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import User from 'App/Models/User'
 import crypto from 'crypto'
 import { DateTime } from 'luxon'
+import Mail from '@ioc:Adonis/Addons/Mail'
+import moment from 'moment'
 
 export default class ForgotPasswordsController {
     async store({ request, response }: HttpContextContract) {
@@ -16,5 +18,44 @@ export default class ForgotPasswordsController {
         user.tokenCreatedAt = DateTime.fromJSDate(new Date())
 
         await user.save()
+
+        await Mail.send(
+            message => {
+                message
+                    .to(user.email)
+                    .from('johnny@adonis.com', 'Johhny | Luby')
+                    .subject('Reset password')
+                    .htmlView('forgot_password', {
+                        email,
+                        token: user.tkn,
+                        link: `${request.input('request_url')}?token=${user.tkn}`
+                    })
+            }
+        )
+    }
+
+    async update({ request, response }: HttpContextContract) {
+        const { token, password } = request.all()
+
+        const user = await User.findBy('tkn', token)
+
+        if (!user) {
+            return response.status(404).send('token cant be found')
+        }
+
+        const tokenExpired = moment()
+            .subtract('7', 'days')
+            .isAfter(user?.tokenCreatedAt)
+
+        if (tokenExpired) {
+            return response.status(401).send('expired token')
+        }
+
+        user.tkn = ''
+        user.tokenCreatedAt = DateTime.fromJSDate(new Date())
+        user.password = password
+
+        await user.save()
+
     }
 }
